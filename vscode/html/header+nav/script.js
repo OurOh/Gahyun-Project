d7248ce095e9b8142a7c55f5106e356761d6a203
Ginie-J <kyuginie@gmail.com>
2024-10-11 17:57:19 +0900

    stylesheet통합 시작
    header, nav 완료. stylesheet 통합 정리 시작

--- vscode/html/header+nav/script.js
+++ vscode/html/header+nav/script.js
@@ -2,5 +2,7 @@ $(function(){
     $(".main-nav").hover(function(){
         $(".nav-col").stop().slideToggle(300);
     });
-
+    $(".ham").click(function(){
+        $(".ham-nav").stop().slideToggle(300);
+    })
 }); //jquery
